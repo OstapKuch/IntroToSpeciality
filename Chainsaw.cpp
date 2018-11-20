33a549024e9df0699c476fa33dcbdd0e9a842a99
OstapKuch <ostapko220@gmail.com>
2018-11-20 16:13:46 +0200

    cleaned my code
    Renamed  variables
    Renamed method getClass

--- Chainsaw.cpp
+++ Chainsaw.cpp
@@ -7,15 +7,15 @@ Chainsaw::Chainsaw() {
     this->name = "Name";
 
 }
-Chainsaw::Chainsaw(string Name, int Power, int Turns, int prot_1, int prot_2, int pub_1, string pub_2) {
+Chainsaw::Chainsaw(string name, int power, int turns, int protected_1, int protected_2, int public_1, string public_2) {
 
-    name = Name;
-    power = Power;
-    turns = Turns;
-    protected_1 = prot_1;
-    protected_2 = prot_2;
-    public_1 = pub_1;
-    public_2 = pub_2;
+    this->name = name;
+    this->power = power;
+    this->turns = turns;
+    this->protected_1 = protected_1;
+    this->protected_2 = protected_2;
+    this->public_1 = public_1;
+    this->public_2 = public_2;
 
 }
 
@@ -24,7 +24,7 @@ Chainsaw::~Chainsaw() {
     std::cout << "This is destructor" << endl;
 
 }
-void Chainsaw::getClass() {
+void Chainsaw::getAllClassFields() {
 
     std::cout << "Name: " << name << " Power: " << power << " Turns: " << turns << " Public_1: " << public_1 << " Public_2: " << public_2 << " Protected_1: " << protected_1 << " Protected_2: " << protected_2 << std::endl;
 
