0e6b6bca0a2d1f6abee1acebdda3a479fcfbe070
OstapKuch <ostapko220@gmail.com>
2018-11-20 16:14:35 +0200

    cleaned code
    Changed name of method getClass

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main() {
     Chainsaw Makita("Makita", 2000, 400, 6, 8, 5, "Makita_2");
     Chainsaw Keyi("Keyi", 1800, 100, 1, 9, 6, "Keyi_453");
     Chainsaw Viper("Viper", 2500, 450, 4, 7, 9, "Viper_326");
-    Makita.getClass();
-    Keyi.getClass();
-    Viper.getClass();
+    Makita.getAllClassFields();
+    Keyi.getAllClassFields();
+    Viper.getAllClassFields();
 
 }
